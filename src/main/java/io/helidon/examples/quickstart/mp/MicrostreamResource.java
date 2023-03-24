561aee98de1ed238d5d4caed5aeb97fa0fa1ba70
Manuel Stör <m.stoer@microstream.one>
2023-03-24 08:15:35 +0100

    Update helidon version

--- src/main/java/io/helidon/examples/quickstart/mp/MicrostreamResource.java
+++ src/main/java/io/helidon/examples/quickstart/mp/MicrostreamResource.java
@@ -1,8 +1,7 @@
 
 package io.helidon.examples.quickstart.mp;
 
-import javax.ws.rs.Path;
-
+import jakarta.ws.rs.Path;
 import one.microstream.cluster.nodelibrary.helidon.StorageClusterController;
 
 @Path("/my-microstream-path")
--- src/main/java/io/helidon/examples/quickstart/mp/PersonResource.java
+++ src/main/java/io/helidon/examples/quickstart/mp/PersonResource.java
@@ -3,21 +3,21 @@ package io.helidon.examples.quickstart.mp;
 
 import java.util.Collections;
 
-import javax.json.Json;
-import javax.json.JsonArray;
-import javax.json.JsonArrayBuilder;
-import javax.json.JsonBuilderFactory;
-import javax.json.JsonObject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-
 import org.eclipse.microprofile.openapi.annotations.media.Content;
 import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+
+import jakarta.json.Json;
+import jakarta.json.JsonArray;
+import jakarta.json.JsonArrayBuilder;
+import jakarta.json.JsonBuilderFactory;
+import jakarta.json.JsonObject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 @Path("/")
 public class PersonResource
