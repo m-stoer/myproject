22f48e9b8f0fd803f75d41dec4f0fd05d31e11e5
Manuel Stör <m.stoer@microstream.one>
2023-03-27 08:37:59 +0200

    Change helidon to parent again

--- src/test/java/io/helidon/examples/quickstart/mp/MainTest.java
+++ src/test/java/io/helidon/examples/quickstart/mp/MainTest.java
@@ -1,94 +1,82 @@
 
 package io.helidon.examples.quickstart.mp;
 
-import org.eclipse.microprofile.metrics.Counter;
-import org.eclipse.microprofile.metrics.MetricRegistry;
-import jakarta.inject.Inject;
-import jakarta.ws.rs.client.WebTarget;
-import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.client.Entity;
-import jakarta.ws.rs.core.MediaType;
-
-import io.helidon.microprofile.tests.junit5.HelidonTest;
-
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.eclipse.microprofile.metrics.Counter;
+import org.eclipse.microprofile.metrics.MetricRegistry;
+import org.junit.jupiter.api.Test;
+
+import io.helidon.microprofile.tests.junit5.HelidonTest;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.client.Entity;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @HelidonTest
-class MainTest {
+class MainTest
+{
+	@Inject
+	private MetricRegistry registry;
+	@Inject
+	private WebTarget target;
 
-    @Inject
-    private MetricRegistry registry;
-    @Inject
-    private WebTarget target;
+	@Test
+	void testMicroprofileMetrics()
+	{
+		String message = this.target.path("simple-greet/Joe").request().get(String.class);
 
+		assertThat(message, is("Hello Joe"));
+		final Counter counter = this.registry.counter("personalizedGets");
+		final double before = counter.getCount();
 
-    @Test
-    void testMicroprofileMetrics() {
-        String message = target.path("simple-greet/Joe")
-                .request()
-                .get(String.class);
+		message = this.target.path("simple-greet/Eric").request().get(String.class);
 
-        assertThat(message, is("Hello Joe"));
-        Counter counter = registry.counter("personalizedGets");
-        double before = counter.getCount();
+		assertThat(message, is("Hello Eric"));
+		final double after = counter.getCount();
+		assertEquals(1d, after - before, "Difference in personalized greeting counter between successive calls");
+	}
 
-        message = target.path("simple-greet/Eric")
-                .request()
-                .get(String.class);
+	@Test
+	void testMetrics()
+	{
+		final Response response = this.target.path("metrics").request().get();
+		assertThat(response.getStatus(), is(200));
+	}
 
-        assertThat(message, is("Hello Eric"));
-        double after = counter.getCount();
-        assertEquals(1d, after - before, "Difference in personalized greeting counter between successive calls");
-    }
-    @Test
-    void testMetrics() {
-        Response response = target
-                .path("metrics")
-                .request()
-                .get();
-        assertThat(response.getStatus(), is(200));
-    }
-    @Test
-    void testHealth() {
-        Response response = target
-                .path("health")
-                .request()
-                .get();
-        assertThat(response.getStatus(), is(200));
-    }
-    @Test
-    void testGreet() {
-        Message message = target
-                .path("simple-greet")
-                .request()
-                .get(Message.class);
-        assertThat(message.getMessage(), is("Hello World!"));
-    }
-                    @Test
-    void testGreetings() {
-        Message jsonMessage = target
-                .path("greet/Joe")
-                .request()
-                .get(Message.class);
-        assertThat(jsonMessage.getMessage(), is("Hello Joe!"));
+	@Test
+	void testHealth()
+	{
+		final Response response = this.target.path("health").request().get();
+		assertThat(response.getStatus(), is(200));
+	}
 
-        try (Response r = target
-                .path("greet/greeting")
-                .request()
-                .put(Entity.entity("{\"greeting\" : \"Hola\"}", MediaType.APPLICATION_JSON))) {
-            assertThat(r.getStatus(), is(204));
-        }
+	@Test
+	void testGreet()
+	{
+		final Message message = this.target.path("simple-greet").request().get(Message.class);
+		assertThat(message.getMessage(), is("Hello World!"));
+	}
 
-        jsonMessage = target
-                .path("greet/Jose")
-                .request()
-                .get(Message.class);
-        assertThat(jsonMessage.getMessage(), is("Hola Jose!"));
-    }
-                }
+	@Test
+	void testGreetings()
+	{
+		Message jsonMessage = this.target.path("greet/Joe").request().get(Message.class);
+		assertThat(jsonMessage.getMessage(), is("Hello Joe!"));
+
+		try (
+			Response r = this.target.path("greet/greeting")
+				.request()
+				.put(Entity.entity("{\"greeting\" : \"Hola\"}", MediaType.APPLICATION_JSON))
+		)
+		{
+			assertThat(r.getStatus(), is(204));
+		}
+
+		jsonMessage = this.target.path("greet/Jose").request().get(Message.class);
+		assertThat(jsonMessage.getMessage(), is("Hola Jose!"));
+	}
+}
