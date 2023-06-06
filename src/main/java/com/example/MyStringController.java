22fb04cb5c5b5810d358cab14b8b2cf440f44236
Manuel Stör <m.stoer@microstream.one>
2023-06-06 11:08:20 +0200

    Magic

--- src/main/java/com/example/MyStringController.java
+++ src/main/java/com/example/MyStringController.java
@@ -9,6 +9,7 @@ import io.micronaut.http.annotation.Delete;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Put;
 import io.micronaut.http.annotation.QueryValue;
+import io.micronaut.http.context.ServerRequestContext;
 
 @Controller("/")
 public class MyStringController
@@ -18,19 +19,31 @@ public class MyStringController
 	@Get
 	public List<String> getAllMyString()
 	{
+		System.out.println("Received get all");
 		return this.myStringDAO.getMyStrings();
 	}
 
 	@Get("/{myString}")
 	public String getMyString(@QueryValue final String myString)
 	{
+		System.out.println("Received get my string");
 		return this.myStringDAO.getMyString(myString);
 	}
 
 	@Put(consumes = MediaType.TEXT_PLAIN)
 	public void postMyString(@Body final String myString)
 	{
-		this.myStringDAO.addMyString(myString);
+		System.out.println("Received post my string");
+		final var writos = ServerRequestContext.currentRequest().get().getHeaders().get("Writos");
+		if (writos != null)
+		{
+			System.out.println("Request has writos with value: " + writos);
+			if(writos == "Pogos")
+			{
+				System.out.println("Correct value, therefore we are savin!");
+				this.myStringDAO.addMyString(myString);
+			}
+		}
 	}
 
 	@Delete("/{myString}")
