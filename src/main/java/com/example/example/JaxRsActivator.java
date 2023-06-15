0095e9cc176025d07ca05c483ac62d285a9410e7
Manuel Stör <m.stoer@microstream.one>
2023-06-15 11:44:37 +0200

    Revert "Testing..."
    This reverts commit 110f9ed1e8051c3fef3c3f28b0a3ff05e77054bd.

--- src/main/java/com/example/example/JaxRsActivator.java
+++ src/main/java/com/example/example/JaxRsActivator.java
@@ -1,12 +1,29 @@
 package com.example.example;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Set;
+
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.ApplicationPath;
-import one.microstream.enterprise.cluster.nodelibrary.common.StorageClusterControllerBase;
 import one.microstream.enterprise.cluster.nodelibrary.helidon.MicrostreamBeanProvider;
 
 @ApplicationScoped
-@ApplicationPath(StorageClusterControllerBase.CONTROLLER_PATH)
+@ApplicationPath("/")
 public class JaxRsActivator extends MicrostreamBeanProvider
 {
+	private Collection<Class<?>> getAppResources()
+	{
+		final var resources = new ArrayList<Class<?>>(1);
+		resources.add(ExampleResource.class);
+		return resources;
+	}
+
+	@Override
+	public Set<Class<?>> getClasses()
+	{
+		final var set = super.getClasses();
+		set.addAll(this.getAppResources());
+		return set;
+	}
 }
