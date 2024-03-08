eb878c9a493d05b9fb31319d6839fa0c6bee61f3
Manuel Stör <m.stoer@microstream.one>
2024-03-08 13:21:49 +0100

    Import nodelib beans

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,8 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import one.microstream.enterprise.cluster.nodelibrary.springboot.MicroStreamCluster;
 
 @SpringBootApplication
+@Import(MicroStreamCluster.class)
 public class DemoApplication
 {
 	public static void main(final String[] args)
