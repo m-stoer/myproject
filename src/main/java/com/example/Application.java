8b88c835fbad6d62c1ffd89e88edb31c3ce2b0a4
Manuel Stör <m.stoer@microstream.one>
2024-10-07 10:05:05 +0200

    ERROR TEST

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -5,6 +5,7 @@ import io.micronaut.runtime.Micronaut;
 public class Application {
 
     public static void main(final String[] args) {
+        I am error :(
         Micronaut.run(Application.class, args);
     }
 }
