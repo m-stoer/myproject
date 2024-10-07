acfadc51952a365bfb40627ff56d3ca5cccd9f60
Manuel Stör <m.stoer@microstream.one>
2024-10-07 12:47:22 +0200

    Fix Error

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -5,7 +5,6 @@ import io.micronaut.runtime.Micronaut;
 public class Application {
 
     public static void main(final String[] args) {
-        I am error :(
         Micronaut.run(Application.class, args);
     }
 }
