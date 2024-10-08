49fe970596c554041625ebf9423c82dbd6de833e
Manuel Stör <m.stoer@microstream.one>
2024-10-08 11:47:27 +0200

    Update Application.java

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -4,7 +4,7 @@ import io.micronaut.runtime.Micronaut;
 
 public class Application {
 
-    public static void main(final String[] args) {wqeqt
+    public static void main(final String[] args) {
         Micronaut.run(Application.class, args);
     }
 }
