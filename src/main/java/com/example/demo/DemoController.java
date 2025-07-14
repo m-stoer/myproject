f627eada734207fabf1836e3d6bcdeb904e14c36
Manuel Stör <m.stoer@microstream.one>
2025-07-14 13:39:31 +0200

    UPdate

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.serializer.concurrency.LockedExecutor;
 import org.springframework.http.MediaType;
 import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,16 +11,19 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import one.microstream.enterprise.cluster.nodelibrary.common.ClusterLockScope;
 import one.microstream.enterprise.cluster.nodelibrary.common.ClusterStorageManager;
+import one.microstream.enterprise.cluster.nodelibrary.common.exception.NotADistributorException;
 
 @RestController
-public class DemoController
+public class DemoController extends ClusterLockScope
 {
 	private final ClusterStorageManager<DataRoot> storage;
 	private final DataRoot root;
 
-	public DemoController(final ClusterStorageManager<DataRoot> storageManager)
+	public DemoController(final ClusterStorageManager<DataRoot> storageManager, final LockedExecutor executor)
 	{
+		super(executor);
 		this.storage = storageManager;
 		this.root = this.storage.root().get();
 	}
@@ -27,6 +31,14 @@ public class DemoController
 	@GetMapping
 	public List<String> getStrings()
 	{
+		this.write(() ->
+		{
+			System.out.println("Hello :)");
+		});
+		if (Math.random() != 69.0)
+		{
+			throw new NotADistributorException();
+		}
 		synchronized (this.storage)
 		{
 			return new ArrayList<>(this.root.strings);
