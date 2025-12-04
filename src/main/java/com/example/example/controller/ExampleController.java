c22d6e5fc83e336e9761e4e2eef813dac5da4a32
Manuel Stör <m.stoer@microstream.one>
2025-12-04 14:29:14 +0100

    Fix imports

--- src/main/java/com/example/example/controller/ExampleController.java
+++ src/main/java/com/example/example/controller/ExampleController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.example.storage.DataRoot;
 
-import one.microstream.enterprise.cluster.nodelibrary.common.ClusterStorageManager;
+import one.microstream.enterprise.cluster.nodelibrary.types.ClusterStorageManager;
 
 @RestController("/")
 public class ExampleController
@@ -22,6 +22,6 @@ public class ExampleController
 	@GetMapping
 	public String root()
 	{
-		return "Data root, created @ " + root;
+		return "Data root, created @ " + this.root;
 	}
 }
